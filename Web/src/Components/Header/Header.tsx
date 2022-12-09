4837886fee9dc851c3b5f051234d5ca8690dc497
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-12-09 16:35:03 -0300

    fix: retira import com desestruturação

--- Web/src/Components/Header/Header.tsx
+++ Web/src/Components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { header } from "./Header.module.css";
+import styles from "./Header.module.css";
 import feedLogo from "../../Assets/newsfeed.png";
 
 export function Header() {
   return (
-    <header className={header}>
+    <header className={styles.header}>
       <img src={feedLogo} alt="Logo Feed" />
       <h1>The Feed</h1>
     </header>
